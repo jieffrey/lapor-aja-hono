d85b8d36f8ea9440729111cc316a8e205f1f403e
jieffrey <kyooon08@gmail.com>
2026-05-10 09:19:19 +0700

    auth-setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Hono } from "hono"
+import authRoute from './routes/auth.route';
 
 const app = new Hono()
 
 app.get("/", (c) => {
   return c.text("LaporAja API Running")
 })
+
+app.route("/auth", authRoute)
+
 
 export default {
   port: Number(process.env.PORT) || 3000,
