8ff109eeca98bd22aa129d0e584709bd8d35b6d1
jieffrey <kyooon08@gmail.com>
2026-06-16 15:44:00 +0700

    migration-supabase

--- src/database/database.ts
+++ src/database/database.ts
@@ -8,7 +8,9 @@ const pool = new Pool ({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-  port: Number(process.env.DB_PORT)
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+  ssl: { rejectUnauthorized: false },
 })
 
 pool.connect()
